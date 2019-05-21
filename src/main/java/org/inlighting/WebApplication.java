9515566f84acac1f1aca91e09bf06ab85c763c05
guozhiyang_vendor <guozhiyang_vendor@sensetime.com>
2019-05-21 19:29:06 +0800

    wangshangxuexi

---
+++ src/main/java/org/inlighting/WebApplication.java
@@ -0,0 +1,13 @@
+package org.inlighting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
